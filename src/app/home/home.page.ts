7c97cb062eab3544d84797f1a65e9a9abfe85013
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-10-13 22:51:12 +0600

    initial calendar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Calendar } from '@ionic-native/calendar/ngx';
+import { NavController, Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  calenders = [];
 
-  constructor() {}
+  constructor(private calender: Calendar,
+    private plt: Platform,
+    private router:Router,
+    private navCtrl: NavController) {
+    this.plt.ready().then(() => {
+      this.calender.listCalendars().then(data => {
+        this.calender = data
+      });
+    });
+  }
+
+  addEvent(cal) {
+    let date = new Date();
+    let options = { calendarId: cal.id, calendarName: cal.name, url: 'https://ionicacademy.com', firstReminderMinutes: 15 }
+
+    this.calender.createEventInteractivelyWithOptions('My new Event', 'Münster', 'Special Notes', date, date, options).then(res => {
+    }, err => {
+      console.log('err: ', err);
+    });
+
+  }
+
+  openCal(cal) {
+    return this.router.navigateByUrl('CalDetailsPage')
+    // this.navCtrl.push('CalDetailsPage', { name: cal.name });
+  }
 
 }
